3e8bc0ce5748978be9b4d0591e90f659820ef88f
Rhett Garber <rhettg@gmail.com>
2021-07-29 19:36:38 +0000

    refactor, faster loop

--- rfm69.py
+++ rfm69.py
@@ -19,31 +19,49 @@ CS = DigitalInOut(board.D5)
 RESET = DigitalInOut(board.D6)
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 
-try:
-    rfm69 = adafruit_rfm69.RFM69(spi, CS, RESET, 915.0)
-    print('RFM69: Detected', 0, 0, 1)
-except RuntimeError as error:
-    # Thrown on version mismatch
-    print('RFM69 Error: ', error)
-    sys.exit(1)
+def initialize_radio():
+    try:
+        rfm69 = adafruit_rfm69.RFM69(spi, CS, RESET, 915.0)
+        print('RFM69: Detected', 0, 0, 1)
+    except RuntimeError as error:
+        # Thrown on version mismatch
+        print('RFM69 Error: ', error)
+        sys.exit(1)
+    return rfm69
 
-last_send = 0
-while True:
-    time.sleep(0.1)
-
-    loop_start = time.time()
+def do_recv(rfm69):
     packet = rfm69.receive()
     if packet is not None:
         packet_text = str(packet, "utf-8")
         print("RX: {}".format(packet_text))
 
-    if time.time() - last_send > 1.0:
+def do_send_ping(rfm69):
         send_data = bytes("hello\r\n","utf-8")
         print("TX: {}", send_data)
         rfm69.send(send_data)
         last_send = time.time()
     
-    loop_duration = time.time() - loop_start
-    if loop_duration > 0.1:
-        print("Loop: {}s".format(loop_duration))
 
+def main():
+    rfm69 = initialize_radio()
+
+    last_send = 0
+    while True:
+        loop_start = time.time()
+
+        if rfm69.payload_ready():
+            do_recv(rfm69)
+
+        if time.time() - last_send > 1.0:
+            do_send_ping(rfm69)
+            last_send = time.time()
+
+        loop_duration = time.time() - loop_start
+        if loop_duration > 0.1:
+            print("Loop: {}s".format(loop_duration))
+
+        if loop_duration < 0.1:
+            time.sleep(0.1)
+
+if __name__ == '__main__':
+    main()
