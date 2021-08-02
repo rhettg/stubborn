85d46c04864da29c3f114bb504818bfdcebb8ca7
Rhett Garber <rhettg@gmail.com>
2021-08-02 01:45:02 +0000

    move to select loop

--- comsock/main.c
+++ comsock/main.c
@@ -5,24 +5,27 @@
 #include <unistd.h>
 #include <errno.h>
 
+#include "evt.h"
+
+EVT_t evt = {0};
+
 void fatal(const char *msg)
 {
     fprintf(stderr, "%s\n", msg);
     exit(1);
 }
 
-int main(int argc, char const *argv[])
+int open_client_sock()
 {
-    char buf[1024];
     struct sockaddr_un addr;
-    int fd, cl;
-    int rl;
+    int fd;
 
     fd = socket(AF_UNIX, SOCK_STREAM, 0);
 
     if (0 != unlink("socket")) {
         if (ENOENT != errno) {
-            fatal("failed to remove");
+            perror("failed to remove");
+            return -1;
         }
     }
 
@@ -31,36 +34,89 @@ int main(int argc, char const *argv[])
     strncpy(addr.sun_path, "socket", sizeof(addr.sun_path)-1);
 
     if (0 != bind(fd, (struct sockaddr*)&addr, sizeof(addr))) {
-        fatal("failed to open socket");
+        perror("failed to open socket");
+        return -1;
     }
 
     if (0 != listen(fd, 1)) {
-        fatal("failed to listen");
+        perror("failed to listen");
+        return -1;
     }
 
+    return fd;
+}
+
+int run_server(int server)
+{
+    char buf[1024];
+    int c_fd = 0;
+    size_t rlen;
+    fd_set set;
+
+    printf("starting server\n");
+
     while (1) {
-        cl = accept(fd, 0, 0);
-        if (cl < 0) {
-            printf("failed to accept: %d\n", errno);
+        FD_ZERO(&set);
+        
+        if (0 == c_fd) {
+            FD_SET(server, &set);
+        } else {
+            FD_SET(c_fd, &set);
+        }
+
+        printf("waiting for io\n");
+        int ret = select(FD_SETSIZE, &set, NULL, NULL, NULL);
+        if (0 > ret) {
+            perror("failed to select");
             continue;
         }
+        printf("io ready\n");
 
-        while(1) {
-            rl = read(cl, buf, sizeof(buf));
-            if (rl < 0 && EAGAIN != errno) {
-                printf("failed reading\n");
-                rl = 0;
+        if (0 != FD_ISSET(server, &set)) {
+            printf("accepting to connection\n");
+            c_fd = accept(server, 0, 0);
+            if (c_fd < 0) {
+                fprintf(stderr, "failed to accept: %d\n", errno);
+                continue;
             }
-
-            if (rl == 0) {
-                printf("closing client\n");
-                close(cl);
-                break;
-            }
-
-            buf[rl] = 0;
-            printf("RX: %s\n", buf);
         }
+
+        if (0 != FD_ISSET(c_fd, &set)) {
+            printf("reading request from client\n");
+            rlen = read(c_fd, buf, sizeof(buf));
+            if (rlen < 0 && EAGAIN != errno) {
+                fprintf(stderr, "failed reading: %d\n", errno);
+                rlen = 0;
+            }
+
+            if (0 < rlen) {
+                buf[rlen] = 0;
+                printf("RX: %s\n", buf);
+            } else {
+                printf("closing client\n");
+                close(c_fd);
+                c_fd = 0;
+            }
+        }
+    }
+
+    return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+    int fd;
+
+    EVT_init(&evt);
+
+    fd = open_client_sock();
+    if (0 > fd) {
+        exit(1);
+    }
+
+    if (0 != run_server(fd)) {
+        fprintf(stderr, "server failed\n");
+        exit(1);
     }
 
     return 0;
